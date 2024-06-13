f183b00ba0005005048f9203edbefa70ca7ab9b6
lokesh903 <lavkeshchhatani@gmail.com>
2024-06-13 20:35:21 +0530

    sorting learning done

--- dsa/test.py
+++ dsa/test.py
@@ -1,9 +1,8 @@
-a = [8, 6, 2, 5, 1]
-for i in range (len(a)-1):
-    mp = i
-    for j in range(i+1,len(a)):
-        if a[mp] < a[j]:
-            mp = j
-    a[mp],a[i] = a[i],a[mp]
-print(a)
+arr=[5,0,7,4,8,1,3,0,7,2,0]
+d = 2
+arr = arr[d:] + arr[:d]
+
+print(arr)
+
+
 
